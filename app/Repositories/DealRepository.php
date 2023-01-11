e1b5e48604b6045c7c3240db946659c93ca3b186
Diego Gonzalez <dgonzalez@eonnet.io>
2023-01-11 11:06:23 -0600

    query bug fixed

--- app/Repositories/DealRepository.php
+++ app/Repositories/DealRepository.php
@@ -142,7 +142,7 @@ class DealRepository
                 $connection = $this->setConnection('PORTAL_WEB');
                 // $res = DB::connection($connection)->update("UPDATE deals SET status = '$dealStage', status_number = $statusNumber where deal_id = ?", [$id]);
                 // $res = DB::connection($connection)->update('UPDATE deals SET status = ?, status_number = ? where deal_id = ?', ["$dealStage" , $statusNumber , $id]);
-                $res = DB::connection($connection)->update(DB::raw('UPDATE deals SET status = ' . $dealStage . ', status_number = ' . $statusNumber . 'where deal_id = ' . $id));
+                $res = DB::connection($connection)->update(DB::raw('UPDATE deals SET status = ' . "$dealStage" . ', status_number = ' . $statusNumber . 'where deal_id = ' . $id));
                 if (!$res)
                 {
                     $message = "La etapa del deal $id no ha sido modificada";
